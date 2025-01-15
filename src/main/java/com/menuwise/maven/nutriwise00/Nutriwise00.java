52ff6d2d4084c97caed13314ead0c4e0b09f7bf3
Elsa Beltran <vivanco.elsa@gmail.com>
2025-01-15 18:36:49 +0000

    inicio y actualizacion Nutriwise00

---
+++ src/main/java/com/menuwise/maven/nutriwise00/Nutriwise00.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.menuwise.maven.nutriwise00;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Nutriwise00 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
