a581f0e261d0dae2769cb0673ade345d5e40c649
Felipe <lfazocar@users.noreply.github.com>
2025-01-16 12:00:11 -0300

    chore: initialize spring boot

---
+++ backend/src/test/java/com/nutriwise/nutriwise/NutriwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriwise.nutriwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
