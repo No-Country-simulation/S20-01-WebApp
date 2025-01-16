2ef43677edb1040dc8c641f94a8d28bc3bc207f2
E-vivanco <vivanco.elsa@gmail.com>
2025-01-16 02:44:58 +0000

    primer commit de inicio

---
+++ PruebaIntegracion/src/main/java/com/nutriwise/maven/pruebaintegracion/PruebaIntegracion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nutriwise.maven.pruebaintegracion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PruebaIntegracion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
